b8805ccf975ef2d33b38a59ae6539b57fd40ff79
Patrick <cognition.crow@gmail.com>
2009-06-22 20:04:52 -0300

    testing git commit using GitGui

--- test/tests_all.rb
+++ test/tests_all.rb
@@ -28,4 +28,59 @@ class TestID3A < Test::Unit::TestCase
 		assert_equal " \"High \"", yml.split("\n")[4].split("Cell").last
 	end
 	
+	def test_marshal_dump
+		if File.exist?("test.rules") then 
+			File.delete("test.rules")
+		end
+
+		@data_set = DataSet.new.load_csv_with_labels '../examples/pool.csv'
+		@id3 = ID3.new.build(@data_set)
+		@id3.get_rules
+		
+		File.open("test.rules", 'w') do |f|
+			Marshal.dump(@id3, f)
+		end
+		
+		assert_equal true, File.exist?("test.rules")
+		File.delete("test.rules")
+	end
+	
+	def test_load_marshal_dump
+		if File.exist?("test.rules") then 
+			File.delete("test.rules")
+		end
+
+		@data_set = DataSet.new.load_csv_with_labels '../examples/pool.csv'
+		@id3 = ID3.new.build(@data_set)
+		@id3.get_rules
+		
+		File.open("test.rules", 'w') do |f|
+			Marshal.dump(@id3, f)
+		end
+		
+		@id3 = nil
+		
+		File.open("test.rules",'r') do |f|
+			@id3 = Marshal.load(f)
+		end
+		
+		File.delete("test.rules") unless !File.exist?("test.rules")
+	end
+	
+	def test_all_pool_examples
+		@data_set = DataSet.new.load_csv_with_labels '../examples/pool.csv'
+		@id3 = ID3.new.build(@data_set)
+		@id3.get_rules
+		
+		assert_equal 'No ', @id3.eval(["Sunny ","Hot","High ","Weak"])
+		assert_equal 'No ', @id3.eval(["Sunny ","Hot","High ","Strong"])
+		assert_equal 'Yes ', @id3.eval(["Overcast ","Hot","High ","Weak"])
+		assert_equal 'Yes ', @id3.eval(["Rain ","Mild","High ","Weak"])
+		assert_equal 'Yes ', @id3.eval(["Rain ","Cool","Normal ","Weak"])
+		assert_equal 'No ', @id3.eval(["Rain ","Cool","Normal ","Strong"])
+		assert_equal 'Yes ', @id3.eval("Overcast ","Cool","Normal ","Strong"])
+		
+	end
+	
 end
+
